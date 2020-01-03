95a1e9596be481934f47999bd0729c0352cca335
victor <victorgotzilla@hotmail.com>
2020-01-02 18:35:46 -0800

    3er ejercicio agregado

--- javascript/03-ejercicio.js
+++ javascript/03-ejercicio.js
@@ -3,6 +3,6 @@
 
 var num1 = parseInt(prompt("introduce un numero", 0));
 var num2 = parseInt(prompt("ingrese otro numero", 0));
-for (var i = num1 + 1; i < num2; i++) {
+for (var i = num1; i <= num2; i++) {
     console.log(i);
 }
