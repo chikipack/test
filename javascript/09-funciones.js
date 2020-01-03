a2e8dfc35168d395b4140667d39548b1456a5fe5
victor <victorgotzilla@hotmail.com>
2020-01-03 01:30:44 -0800

    functions added

---
+++ javascript/09-funciones.js
@@ -0,0 +1,10 @@
+'use strict'
+//functions
+
+function calculadora() {
+    console.log("hola soy la calculadora");
+    console.log("simon");
+    //return "hola soy una calculadora";
+}
+
+calculadora();
